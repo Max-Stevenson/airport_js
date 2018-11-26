8bee7b1b05400f50af871482ac565b5746967609
Max Stevenson <maxstevenson@msn.com>
2018-11-26 16:43:45 +0000

    can land planes

---
+++ jasmine/jasmine-standalone-3.3.0 (1)/src/airport.js
@@ -0,0 +1,16 @@
+class Airport {
+	constructor() {
+		this.landedPlanes = []
+	}
+	land(plane) {
+		this.landedPlanes.push(plane)
+	}
+	// land_plane = function(plane) {
+	// 	landedPlanes.push(plane)
+	// }
+};
+
+
+	// landed_planes: [],
+	// accept_plane: function(plane){
+	// 	this.landed_planes.push(plane);
---
+++ jasmine/jasmine-standalone-3.3.0 (1)/src/plane.js
@@ -0,0 +1,12 @@
+class Plane {
+	constructor() {
+
+		
+	}
+	land(airport) {
+		airport.land(this)
+	}
+	// land = function(airport) {
+	// 	airport.land_plane(this)
+	// }
+};
